8373c5cdf048719f5193f67779e210858d231756
Ton Nom <ton@email.com>
2026-02-04 13:37:25 +0000

    fix: base path for vercel + github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/souad-resedence-/',
+      base: env.GITHUB_PAGES === 'true' ? '/souad-resedence-/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
